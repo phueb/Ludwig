af49ede8d10c748dcc9399c9c5aac22dbb62e87a
phueb <phueb001@ucr.edu>
2019-02-06 21:13:54 -0600

    require specifying project_name when killing jobs on workers (python executables now have unique file names)

--- scripts/upload_watchers.sh
+++ scripts/upload_watchers.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 
-
 for hostname in lecun bengio hawkins hebb hinton hoff  norman pitts;
 do
     echo Uploading watcher to ${hostname}
@@ -9,8 +8,8 @@ do
     scp ../watcher.py ${hostname}:/var/sftp/LudwigCluster/watcher.py
     scp ../ludwigcluster/config.py ${hostname}:/var/sftp/LudwigCluster/ludwigcluster/config.py
 
-    echo Killing run.py
-    ssh ${hostname} 'pkill -9 -f run.py'
+    echo Killing run_${1}.py
+    ssh ${hostname} "pkill -9 -f run_${1}.py"
 
     echo Killing active watchers
     ssh ${hostname} 'pkill --full --echo "python3 watcher.py"'
