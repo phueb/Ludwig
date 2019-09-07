779bd67643fb49ce0ae5f9b8e5c10b78c70c3e30
phueb <info@philhuebner.com>
2019-09-07 12:25:26 -0500

    move methods from utils to Client class

--- ludwigcluster/client.py
+++ ludwigcluster/client.py
@@ -2,6 +2,7 @@
 The client is used to submit jobs to one or more nodes in LudwigCluster.
 It uses an sftp client library to upload all files in a user's project to LudwigCluster.
 """
+from itertools import cycle
 from pathlib import Path
 import pysftp
 import platform
@@ -20,11 +21,10 @@ from ludwigcluster.logger import Logger
 DISK_USAGE_MAX = 90
 
 
-# TODO rename all configs_dict occurrences to params
-
 class Client:
-    def __init__(self, project_name):
+    def __init__(self, project_name, param2default):
         self.project_name = project_name
+        self.param2default = param2default
         self.hostname2ip = self.make_hostname2ip()
         self.logger = Logger(project_name)
         self.num_workers = len(config.SFTP.worker_names)
@@ -85,14 +85,71 @@ class Client:
             raise SystemExit('{} replications of each model already exist.'.format(reps))
         return res
 
-    def submit(self, src_ps, param2val_list, data_ps=None, reps=1, test=True, worker=None, run_f_name='run.py'):
+    @staticmethod
+    def _iter_over_cycles(param2opts):
+        """
+        return list of mappings from param name to integer which is index to possible param values
+        all possible combinations are returned
+        """
+        lengths = []
+        for k, v in param2opts:
+            lengths.append(len(v))
+        total = np.prod(lengths)
+        num_lengths = len(lengths)
+
+        # cycles
+        cycles = []
+        prev_interval = 1
+        for n in range(num_lengths):
+            l = np.concatenate([[i] * prev_interval for i in range(lengths[n])])
+            if n != num_lengths - 1:
+                c = cycle(l)
+            else:
+                c = l
+            cycles.append(c)
+            prev_interval *= lengths[n]
+
+        # iterate over cycles, in effect retrieving all combinations
+        param_ids = []
+        for n, i in enumerate(zip(*cycles)):
+            param_ids.append(i)
+        assert sorted(list(set(param_ids))) == sorted(param_ids)
+        assert len(param_ids) == total
+        return param_ids
+
+    def list_all_param2vals(self, param2requests, update_d=None, add_names=True):
+        # complete partial request made by user
+        full_request = {k: [v] if k not in param2requests else param2requests[k]
+                        for k, v in self.param2default.copy().items()}
+        #
+        param2opts = tuple(full_request.items())
+        param_ids = self._iter_over_cycles(param2opts)
+
+        res = []
+        for ids in param_ids:
+            # map param names to integers corresponding to which param value to use
+            param2val = {k: v[i] for (k, v), i in zip(param2opts, ids)}
+            if add_names:
+                param2val.update({'param_name': None, 'job_name': None})
+            if isinstance(update_d, dict):
+                param2val.update(update_d)
+
+            res.append(param2val)
+        return res
+
+    def submit(self, src_ps, param2requests, data_ps=None, reps=1, test=True, worker=None, run_f_name='run.py'):
         self.check_lab_disk_space()
+
+        # make list of hyper-parameter configurations to submit
+        param2val_list = self.list_all_param2vals(param2requests)
+
         # copy data to file server
         for data_p in data_ps:
             src = str(data_p)
             dst = str(config.Dirs.research_data / self.project_name / data_p.name)
             print('Copying data in {} to {}'.format(src, dst))
             copy_tree(src, dst)
+
         # add param_name to param2val
         print('Assigning param_names...')
         for n, param2val in enumerate(param2val_list):
@@ -100,11 +157,14 @@ class Client:
             param2val['param_name'] = param_name
             print('param2val {}/{} assigned to {} "{}"'.format(n + 1, len(param2val_list), old_or_new, param_name))
             sys.stdout.flush()
+
         # add reps
         param2val_list = self.add_reps(param2val_list, reps)
+
         # distribute expensive jobs approximately evenly across workers
         param2val_list = np.random.permutation(param2val_list)
         sys.stdout.flush()
+
         # split into chunks (one per node)
         worker_names = iter(np.random.permutation(config.SFTP.worker_names)) if worker is None else iter([worker])
         num_workers = 1 if worker is not None else self.num_workers
@@ -117,24 +177,29 @@ class Client:
             if len(param2val_chunk) == 0:
                 print('Not submitting to {}'.format(worker_name))
                 continue
+
             # add job_name to each param2val
             base_name = self.make_job_base_name(worker_name)
             for n, param2val in enumerate(param2val_chunk):
                 job_name = '{}_num{}'.format(base_name, n)
                 param2val['job_name'] = job_name
+
             # save chunk to shared drive (after addition of job_name)
             p = config.Dirs.research_data / self.project_name / '{}_param2val_chunk.pkl'.format(worker_name)
             with p.open('wb') as f:
                 pickle.dump(param2val_chunk, f)
+
             # console
             print('Connecting to {}'.format(worker_name))
             for param2val in param2val_chunk:
                 print(param2val)
+
             # connect via sftp
             sftp = pysftp.Connection(username='ludwig',
                                      host=self.hostname2ip[worker_name],
                                      private_key=self.private_key,
                                      private_key_pass=self.private_key_pass)
+
             # upload src code to worker
             for p in src_ps:
                 localpath = str(p)
@@ -146,8 +211,48 @@ class Client:
             if test:
                 print('Test successful. Not uploading run.py.')
                 continue
+
             # upload run.py
             sftp.put(localpath=run_f_name,
                      remotepath='{}/{}'.format(self.ludwig, 'run_{}.py'.format(self.project_name)))
             print('--------------')
             print()
+
+    def gen_param_ps(self, param2requests, runs_p=None, label_params=None):
+        """
+        Return path objects that point to folders with job results.
+         Folders located in those paths are each generated with the same parameter configuration.
+         Use this for retrieving data after a job has been completed
+        """
+        print('Requested:')
+        print(param2requests)
+        print()
+
+        label_params = set([param for param, val in param2requests.items()
+                            if val != self.param2default[param]] + (label_params or []))
+
+        requested_param2vals = self.list_all_param2vals(param2requests, add_names=False)
+
+        if runs_p is None:
+            runs_p = config.Dirs.research_data / self.project_name / 'runs'
+        for param_p_ in runs_p.glob('param_*'):
+            print('Checking {}...'.format(param_p_))
+
+            # load param2val
+            with (param_p_ / 'param2val.yaml').open('r') as f:
+                param2val = yaml.load(f, Loader=yaml.FullLoader)
+            loaded_param2val = param2val.copy()
+            del loaded_param2val['param_name']
+            del loaded_param2val['job_name']
+
+            # is match?
+            if loaded_param2val in requested_param2vals:
+                label_ = '\n'.join(['{}={}'.format(param, param2val[param]) for param in label_params])
+                label_ += '\nn={}'.format(len(list(param_p_.glob('*num*'))))
+                print('Param2val matches')
+                print(label_)
+                yield param_p_, label_
+            else:
+                print('Params do not match')
+
+
--- submit.py
+++ submit.py
@@ -2,9 +2,9 @@ import argparse
 
 from ludwigcluster.client import Client
 from ludwigcluster.config import SFTP
-from ludwigcluster.utils import list_all_param2vals
 
-from yourmodule import config as yourmoduleconfig
+from yourmodule.params import param2requests, param2default
+from yourmodule import config
 from yourmodule.job import pre_processing_job
 
 
@@ -12,34 +12,35 @@ if __name__ == '__main__':
     # parse cmd-line args
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--reps', default=2, action='store', dest='reps', type=int,
-                        choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], required=False,
+                        choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50], required=False,
                         help='Number of replications to train per hyper-param configuration')
     parser.add_argument('-w', '--worker', default=None, action='store', dest='worker',
                         choices=SFTP.worker_names, required=False,
                         help='Specify a single worker name if submitting to single worker only')
+
+    # TODO add option to specify multiple workers
+
     parser.add_argument('-s', '--skip_data', default=False, action='store_true', dest='skip_data', required=False,
                         help='Whether or not to skip uploading data to file-server. ')
     parser.add_argument('-t', '--test', action='store_true', dest='test', required=False,
                         help='For debugging/testing purpose only')
     parser.add_argument('-p', '--preprocess', action='store_true', default=False, dest='preprocess', required=False,
                         help='Whether to save results of pre-processing job to file-server')
+    parser.add_argument('-d', '--debug', action='store_true', default=False, dest='debug', required=False,
+                        help='Debugging. Minimal param configuration')
     namespace = parser.parse_args()
 
     # preprocess any data and save pickle to file server
     if namespace.preprocess:
         pre_processing_job()
 
-    # make list of hyperparameter configurations to submit
-    param2val_list = list_all_param2vals(Params)
-
-    SFTP.worker_names = SFTP.worker_names  # use this to specify workers (in case one is offline)
-
     # submit to cluster
     data_dirs = [] if not namespace.skip_data else []  # this data is copied to file server not workers
-    client = Client(yourmoduleconfig.RemoteDirs.root.name)
-    client.submit(src_ps=[yourmoduleconfig.LocalDirs.src],
-                  data_ps=[yourmoduleconfig.LocalDirs.root / d for d in data_dirs],
-                  param2val_list=param2val_list,
+    project_name = config.RemoteDirs.root.name
+    client = Client(project_name, param2default)
+    client.submit(src_ps=[config.LocalDirs.src],
+                  data_ps=[config.LocalDirs.root / d for d in data_dirs],
+                  param2requests=param2requests,
                   reps=namespace.reps,
                   test=namespace.test,
                   worker=namespace.worker)
