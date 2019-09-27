c139cac864d06967620a6c4b96fa0d897ecd95f3
phueb <info@philhuebner.com>
2019-09-26 21:59:49 -0500

    bump version to 1.0 and use this commit as first release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ludwigcluster',
-    version='0.2',
+    version='1.0',
     packages=['ludwigcluster'],
     install_requires=['psutil',
                       'pysftp',
