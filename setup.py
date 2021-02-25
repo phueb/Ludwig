1c1eb2e6c9e89c1e4c2b27c767aa982fe6ee0b55
phueb <info@philhuebner.com>
2021-02-24 18:15:01 -0600

    fix setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['psutil',
                       'pysftp',
                       'watchdog',
-                      'paramiko'
+                      'paramiko==2.6.0',
                       'numpy',
                       'pandas',
                       'cached_property', 'PyYAML'],
